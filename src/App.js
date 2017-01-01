2f9133c04f26e3c20182f5f0470eb7b0b5bfe463
steven dale <lifeinchords@gmail.com>
2016-12-31 23:46:49 -0500

    adding a paragraph component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, {Component} from 'react';
+import Paragraph from './Paragraph';
 
-class App extends Component {
+export default class App extends React.Component {
   render() {
-    return (<h1>ooohh yehh!!</h1>);
+    return (
+      < div className="my-app">
+        <h1>This is React!!!</h1>
+        <Paragraph text="First Paragraph" />
+        <Paragraph text="Second Paragraph" />
+      </div>
+    );
   }
 }
-export default App;
